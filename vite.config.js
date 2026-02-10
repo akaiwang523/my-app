e8174548e705e84e1b1b671d6d0e0f787da578a4
akaiwang523 <akaiwang523@gmail.com>
2026-02-10 18:51:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-app/', // 名字要跟你的 GitHub 專案名稱一樣
+  base: '/my-app/', 
   plugins: [react()],
 })
